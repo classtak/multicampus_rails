1511e372f0f2362c48bfc575ae5382b335221ec9
classtak <classtak@gmail.com>
2017-12-12 16:10:13 +0900

    day2 코드

---
+++ W2/Day2/README.md
@@ -0,0 +1,14 @@
+#### sinatra 실습
+
+##### 만들어야 할 파일
+1. app.rb
+2. .erb 파일 두개
+  * erb : form (사용자에게 입력 받는 곳)
+  * erb : 결과물 보여주는 곳
+
+##### 실습 단계
+1. app.rb : sinatra를 쓰겠다.
+2. app.rb : 주소 한 개를 만든다. '/vote'
+3. form 을 가진 erb 파일을 만든다.
+4. app.rb : form 받아오는 주소를 만든다. '/result'
+5. 결과를 보여준다.
---
+++ W2/Day2/app.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+# gem install reloader
+require 'sinatra/reloader'
+require 'csv'
+require 'date'
+# 크롤링을 위한 gem
+require 'nokogiri'
+require 'httparty'
+require 'uri'
+
+get '/vote' do
+  erb :vote
+end
+
+get '/result' do
+  @count = params[:count]
+  erb :result
+end
+
+get '/lolfinder' do
+  erb :lolfinder
+end
+
+get '/lol' do
+  @username = params[:user]
+  url = "http://www.op.gg/summoner/userName=#{@username}"
+  url = URI.encode(url) #한글 url을 인코딩한다.
+  response = HTTParty.get(url) #HTTParty를 활용하여 요청보낸다.
+  doc = Nokogiri::HTML(response) #Nokogiri로 검색하기 편하게 바꾼다.
+  @win = doc.css("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins")
+  @lose = doc.css("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div.SummonerRatingMedium > div.TierRankInfo > div.TierInfo > span.WinLose > span.losses")
+  erb :lol
+end
+
+get '/' do
+  erb :index
+end
+
+get '/welcome/:name' do
+  @name = params[:name]
+  @time = Time.now.to_s
+  # CSV는 require 'csv'
+  CSV.open("user.csv", "a+") do |csv|
+    # Time.now를 쓰려면 require 'date'
+    csv << [@name.to_s, @time]
+  end
+  erb :welcome
+end
+
+get '/users' do
+  @users = []
+  CSV.foreach('user.csv') do |row|
+    @users << row
+  end
+  erb :users
+end
