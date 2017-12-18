18164fd123df00c7ca404828cbf95d89e3ca6652
classtak <classtak@gmail.com>
2017-12-18 04:45:30 +0000

    rails 시작

---
+++ w3/Day1/firstrails/app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  # 컨트롤러는 액션의 모음이다.
+  # 모델과 뷰 사이의 중간관리자이다.
+  # action
+  # app/views/컨트롤러이름/액션이름.html.erb
+  # app/views/home/index.html.erb
+  def index
+  end
+  # action
+  # app/views/home/lotto.html.erb
+  def lotto
+    @lotto = (1..45).to_a.sample(6).sort
+  end
+
+  def welcome
+    @name = params[:name]
+  end
+end
