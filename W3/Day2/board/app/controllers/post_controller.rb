766d0da529eb85da35c93c353fef90d94757ab5b
classtak <classtak@gmail.com>
2017-12-19 10:51:53 +0900

    CRUD - C

---
+++ W3/Day2/board/app/controllers/post_controller.rb
@@ -0,0 +1,25 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    # form에서 글쓰는 창이니까, 아무것도 없다.
+  end
+
+  def create
+    # params[:username]  params[:title] params[:content]
+    # 1번 방법
+    Post.create(:username => params[:username],
+              :title => params[:title],
+              :content => params[:content])
+    # 2번 방법
+    post = Post.new
+    post.username = params[:username]
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+
+    redirect_to '/'
+  end
+end
